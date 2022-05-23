588445fbc87bac3e50f6b4c846fa4b2fb85ac197
trimuru2022 <106047130+trimuru2022@users.noreply.github.com>
2022-05-23 14:57:17 +0700

    Update winXP32.sh

--- winXP32.sh
+++ winXP32.sh
@@ -16,7 +16,7 @@ sudo qemu-system-x86_64 \
   -m 8G \
   -cpu EPYC \
   -boot order=d \
-  -drive file=windows7.iso,media=cdrom \
+  -drive file=windowsXP.iso,media=cdrom \
   -drive file=windows.img,format=raw \
   -drive file=virtio-win.iso,media=cdrom \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
