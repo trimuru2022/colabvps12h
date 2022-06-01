d43bd05750f74f3b3585f1957409b3d49edacf40
trimuru2022 <106047130+trimuru2022@users.noreply.github.com>
2022-06-01 11:03:48 +0700

    Update win7ul32.sh

--- win7ul32.sh
+++ win7ul32.sh
@@ -1,4 +1,4 @@
-get https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 rm -rf ngrok-stable-linux-amd64.zip
 read -p "Enter your ngrok token: " TOKEN
